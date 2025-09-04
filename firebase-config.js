ab0c5d18f0512b0b07d96fa9480913a79def4d9f
kailashbiswal1992 <64208998+kailashbiswal1992@users.noreply.github.com>
2025-09-04 14:30:23 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDykGxIq2GUDxFkv28NLmdRmiQzlEjvFuw",
+  authDomain: "rento-56bca.firebaseapp.com",
+  projectId: "rento-56bca",
+  storageBucket: "rento-56bca.appspot.com",
+  messagingSenderId: "641174222477",
+  appId: "1:641174222477:web:583370943c737c015d14c4",
+  measurementId: "G-G2S1HW7ZNT"
+};
